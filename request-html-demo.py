f5037b53851fc693e54f41a87f9d1b7dbdd2c624
defnngj <defnngj@gmail.com>
2021-09-27 22:27:07 +0800

    feat: 下载和保存图片

--- request-html-demo.py
+++ request-html-demo.py
@@ -3,9 +3,13 @@ author: bugmaster
 data: 2021-09-25
 function: 爬取 豆瓣 top250 电影信息
 """
+import os
 import sqlite3
 from requests_html import HTMLSession
 session = HTMLSession()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+IMG_DIR = os.path.join(BASE_DIR, "static", "images")
 
 
 def save_db(name, img_url, grade, number):
@@ -21,6 +25,22 @@ def save_db(name, img_url, grade, number):
     conn.close()
 
 
+def download_img(img_path, img_name):
+    """
+    保存图片
+    :param img_path:
+    :param img_name:
+    :return:
+    """
+    try:
+        req_pic = session.get(img_path)
+        save_pic = os.path.join(IMG_DIR, img_name)
+        with open(save_pic, "wb") as fp:
+            fp.write(req_pic.content)
+    except Exception as msg:
+        print("下载中出现异常:%s" % str(msg))
+
+
 def get_top_250():
     """
     获取电影：top250
@@ -33,14 +53,16 @@ def get_top_250():
         evaluates = r.html.find('.star > span:nth-child(4)')
         for i in range(25):
             name = imgs[i].attrs["alt"]
-            img = imgs[i].attrs["src"]
+            img_path = imgs[i].attrs["src"]
             grade = stars[i].text
             number = int(evaluates[i].text[0:-3])
             # print(name)
-            # print(img)
+            # print(img_path)
             # print(grade)
             # print(number)
-            save_db(name, img, grade, number)
+            img_name = img_path.split("/")[-1]
+            download_img(img_path, img_name)
+            save_db(name, img_name, grade, number)
 
 
 if __name__ == '__main__':
